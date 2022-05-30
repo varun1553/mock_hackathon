be9707a673bb1a740edeca4fbdef17a412f30239
varsha-66 <varsha.maddika@gmail.com>
2022-05-30 16:49:16 +0530

    reg_creation

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{ useState } from 'react';
 import {useForm} from "react-hook-form";
 
 // import {Form,Button,Container} from 'react-bootstrap';
@@ -11,14 +11,40 @@ const Signin = () => {
        
 
     }
+    const [options,setoption]=useState('');
+
+   const handleChange=(e)=>{
+       setoption( e.target.value);
+    }
   return (
     
 
-            <div className="">
+            <div className="mb-3">
+                {/* <div className='mb-3'>
+                <div className='form-check'>
+                <input type="radio" id="donor" name="options" className='form-check-input' {...register('Donor')} value="donor" onChange={handleChange}></input>
+                <label htmlFor='donor' className='form-check-label'>Donor</label>
+                </div>
+                <div className='form-check'>
+                <input type="radio" id="org" className='form-check-input' {...register('org')}value="org" onChange={handleChange}></input>
+                <label htmlFor='organization' name="options" className='form-check-label'>Organization</label>
+                </div>
+                </div> */}
                 
 
                 <form className='w-50 mx-auto' onSubmit={handleSubmit(onFormSubmit)}>
+
                     <div className="mb-3">
+                    <div className='mb-3'>
+                <div className='form-check'>
+                <input type="radio" id="donor" name="options" className='form-check-input' {...register('Donor')} value="donor" onChange={handleChange} ></input>
+                <label htmlFor='donor' className='form-check-label'>Donor</label>
+                </div>
+                <div className='form-check'>
+                <input type="radio" id="org" className='form-check-input' {...register('org')} value="org" onChange={handleChange} ></input>
+                <label htmlFor='organization' name="options" className='form-check-label'>Organization</label>
+                </div>
+                </div>
                         <label htmlFor="username">UserName</label>
                         <input className='form-control' type="text"  {... register("username",{required:true})} id="username"  />
                         {errors.username?.type=="required"&& <p className='text-danger'>*username is required</p>}
